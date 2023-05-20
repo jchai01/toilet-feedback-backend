1ccad0c997f78a98f630abe93135ca73c735d448
Joseph <josephchai100@hotmail.com>
2023-05-20 17:54:16 +0800

    update send script

--- scripts/send.sh
+++ scripts/send.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo AT+SEND=2:12345678 > /dev/ttyS5
+#
+# echo "l$1r$2"
+# echo "$1,$2"
+
+# if [[ $1 == 1 ]]
+# then
+#   echo "test"
+# fi
+
+echo AT+SEND=2:$1,$2 > /dev/ttyS8
+# echo AT+SEND=2:l$1r$2 > /dev/ttyS8
+# echo AT+SEND=2:12345678 > /dev/ttyS8
